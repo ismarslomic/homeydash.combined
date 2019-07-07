815665f8f9b513e8a11d5c03c6eaa862d933e880
Ismar Slomic <ismar.slomic@vegvesen.no>
2019-07-07 19:50:56 +0200

    Override default HTML meta tags set by webpack

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    chainWebpack: (config) => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'homeydash.combined';
+                args[0].meta = {
+                    viewport: 'width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no',
+                    description: 'Homey Dashboard for iPad/iPhone/e-ink devices'
+                };
+                return args;
+            })
+    },
+    pwa: {
+        name: 'homeydash.combined',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+    }
+};

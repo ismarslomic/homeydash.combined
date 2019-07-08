0a859367ffc87945fc89122af09c868425186bd7
Ismar Slomic <ismar.slomic@vegvesen.no>
2019-07-08 23:02:38 +0200

    Remove hash in router and use history mode

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import Home from './views/Home.vue';
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -18,6 +19,10 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '*',
+      component: () => import(/* webpackChunkName: "notfound" */ './views/NotFound.vue'),
     },
   ],
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="notfound">
+        <h1>Ooops, this page is not found (404)</h1>
+    </div>
+</template>

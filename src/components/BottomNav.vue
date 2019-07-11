58106f10d5e1ef18defde73e7b73ee0b3459008a
Ismar Slomic <ismar.slomic@vegvesen.no>
2019-07-11 23:10:27 +0200

    Add bottom sheet for overflow nav buttons

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -3,16 +3,45 @@
     <v-bottom-navigation
             fixed
             :value="true"
-            shift
             flat
             app
             grow>
-        <template v-for="(item, index) in navItems">
-            <v-btn :to="item.route">
+        <template v-for="(item) in navItems">
+            <v-btn :to="item.route" :class="item.visibilityClassNav">
                 <span>{{item.title}}</span>
                 <v-icon>{{item.icon}}</v-icon>
             </v-btn>
         </template>
+
+        <v-bottom-sheet v-model="sheet">
+            <template v-slot:activator="{ on }">
+                <v-btn
+                        v-on="on"
+                        class="hidden-lg-and-up"
+                        v-model="sheet"
+                >
+                    <span>More</span>
+                    <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+            </template>
+            <v-list>
+                <v-list-item
+                        v-for="(item) in filterItems "
+                        :key="item.title"
+                        @click="sheet = false"
+                        :class="item.visibilityClassOverflow"
+                        :to="item.route"
+                >
+                    <v-list-item-avatar>
+                        <v-avatar size="32px" tile>
+                            <v-icon>{{item.icon}}</v-icon>
+                        </v-avatar>
+                    </v-list-item-avatar>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-bottom-sheet>
+
     </v-bottom-navigation>
 
 </template>
@@ -22,51 +51,72 @@ import {Component, Vue} from 'vue-property-decorator';
 
 @Component
 export default class BottomNav extends Vue {
+    sheet: boolean = false;
     navItems = [
         {
             title: 'Dashboard',
             route: { path: '/'},
-            icon: 'mdi-view-dashboard-outline'
+            icon: 'mdi-view-dashboard-outline',
+            visibilityClassNav: '',
+            visibilityClassOverflow: ''
         },
         {
             title: 'Weather',
             route: { name: 'weather'},
-            icon: 'mdi-weather-partlycloudy'
+            icon: 'mdi-weather-partlycloudy',
+            visibilityClassNav: '',
+            visibilityClassOverflow: ''
         },
         {
             title: 'Climate',
             route: { name: 'climate'},
-            icon: 'mdi-thermometer'
+            icon: 'mdi-thermometer',
+            visibilityClassNav: '',
+            visibilityClassOverflow: ''
         },
         {
             title: 'Security',
             route: { name: 'security'},
-            icon: 'mdi-lock-open-outline'
+            icon: 'mdi-lock-open-outline',
+            visibilityClassNav: 'hidden-sm-and-down',
+            visibilityClassOverflow: 'hidden-md-and-up'
         },
         {
             title: 'Lightning',
             route: { name: 'lightning'},
-            icon: 'mdi-lightbulb-outline'
+            icon: 'mdi-lightbulb-outline',
+            visibilityClassNav: 'hidden-sm-and-down',
+            visibilityClassOverflow: 'hidden-md-and-up'
         },
         {
             title: 'Devices',
             route: { name: 'devices'},
-            icon: 'mdi-access-point-network'
+            icon: 'mdi-access-point-network',
+            visibilityClassNav: 'hidden-md-and-down',
+            visibilityClassOverflow: 'hidden-lg-and-up'
         },
         {
             title: 'Flows',
             route: { name: 'flows'},
-            icon: 'mdi-arrow-decision-auto'
+            icon: 'mdi-arrow-decision-auto',
+            visibilityClassNav: 'hidden-md-and-down',
+            visibilityClassOverflow: 'hidden-lg-and-up'
         },
         {
             title: 'Energy',
             route: { name: 'energy'},
-            icon: 'mdi-power-plug'
-        },
-        {
-            title: 'More',
-            icon: 'mdi-dots-vertical'
-        },
+            icon: 'mdi-power-plug',
+            visibilityClassNav: 'hidden-md-and-down',
+            visibilityClassOverflow: 'hidden-lg-and-up'
+        }
     ];
+
+
+    get filterItems() {
+       return this.navItems.filter( (item) => {
+           return item.visibilityClassNav;
+       });
+    }
+
 }
 </script>

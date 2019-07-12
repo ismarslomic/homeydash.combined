a90c141bdeb607b82b402977a1339d9d76af9912
Ismar Slomic <ismar.slomic@vegvesen.no>
2019-07-12 22:57:57 +0200

    Show icon before content in v-list bottom nav

--- src/components/BottomNav.vue
+++ src/components/BottomNav.vue
@@ -22,12 +22,12 @@
                                      @click="sheet = false"
                                      :class="item.visibilityClassOverflow"
                                      :to="item.route">
-                            <v-list-tile-content>
-                                <v-list-tile-title>{{ item.title  }}</v-list-tile-title>
-                            </v-list-tile-content>
                             <v-list-tile-action>
                                 <v-icon>{{ item.icon}}</v-icon>
                             </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>{{ item.title  }}</v-list-tile-title>
+                            </v-list-tile-content>
                         </v-list-tile>
                     </v-list>
                 </v-bottom-sheet>
